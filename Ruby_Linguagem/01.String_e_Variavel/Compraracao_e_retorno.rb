4fe9f10050e2f49dcc7ec59259f11683d68f7793
levi <levisantosluna@gmail.com>
2022-09-02 22:09:36 -0300

    Comparação e retorno

---
+++ Ruby_Linguagem/01.String_e_Variavel/Compraracao_e_retorno.rb
@@ -0,0 +1,6 @@
+#Retorna o índice Inteiro da primeira substring que corresponde ao dado regexp, ou nada se nenhuma correspondência for encontrada:
+
+puts 'Levi' =~ /e/ # => 0
+puts 'Levi' =~ /L/ # => 1
+puts 'Levi' =~ /x/ # => nil
+
