672b9bfafc022f303f909dbc25d2f4694ca8cc3a
levi <levisantosluna@gmail.com>
2022-09-13 21:42:10 -0300

    corrigindo detalhes cadastro

--- Ruby_Linguagem/10.Métodos/cadastro.rb
+++ Ruby_Linguagem/10.Métodos/cadastro.rb
@@ -8,7 +8,6 @@ introducao
  puts "Responda algumas perguntas",""
  
  print "Seu nome: "
- cadastro << nome
  nome = gets.chomp
 
  if nome.length < 4
@@ -17,10 +16,7 @@ introducao
     puts "Excedeu ao máximo"
  end
 
- 
-
  print "Qual a sua idade: "
- cadastro << idade
  idade = gets.chomp.to_i
 
  if idade <= 0
@@ -29,58 +25,55 @@ introducao
     puts "Você é velinho!"
  end
 
- 
+ print "Qual a sua altura: "
+ altura = gets.chomp.to_f
 
- puts cadastro
- #print "Qual a sua altura: "
- #altura = gets.chomp.to_f
-#
- #if altura <= 0  
- # puts "Esta altura não exeste!"
- #end
-#
- #puts "Qual é o seu sexo"
- #print "(M) Masculino e (F) Feminino :"
- #sexo = gets.chomp
-#
- #if sexo == "m"
- #   puts "Masculino"  
- #  elsif sexo == "f"
- #   puts "Feminino" 
- #  else
- #   puts "Sexo inexistente"
- #end
-#
- #puts "Nos informe o seu endereço: ",""
-#
- #print "Rua: "
- #rua = gets.chomp
-#
- #if rua > 3 
- #   puts "Pouco caracteres para Rua."
- #  elsif rua < 20
- #   puts "Quantidade assima do máximo para Rua."
- #end 
-#
- #print "Número: "
- #numero = gets.chomp.to_i
-#
- #if numero > 0 
- #   puts "Número insuficiente"
- #end
-#
- #print "Cep: "
- #cep = gets.chomp.to_f
-#
- #if cep > 0
- #   puts "Incira o Cep."
- #  elsif cep < 9
- #   puts "Cep inesistente."
- #end
-#
- #puts "Nome: #{nome}"
- #puts "Idade: #{idade}"
- #puts "Altura: #{altura}"
- #puts "Sexo: #{sexo}"
- #puts "Rua: #{rua}"
- #puts "Número: #{numero}"
+ if altura <= 0  
+  puts "Esta altura não exeste!"
+ end
+
+ puts "Qual é o seu sexo"
+ print "(M) Masculino e (F) Feminino :"
+ sexo = gets.chomp
+
+ if sexo == "m"
+    puts "Masculino"  
+   elsif sexo == "f"
+    puts "Feminino" 
+   else
+    puts "Sexo inexistente"
+ end
+
+ puts "Nos informe o seu endereço: ",""
+
+ print "Rua: "
+ rua = gets.chomp
+
+ if rua > 3 
+    puts "Pouco caracteres para Rua."
+   elsif rua < 20
+    puts "Quantidade assima do máximo para Rua."
+ end 
+
+ print "Número: "
+ numero = gets.chomp.to_i
+
+ if numero > 0 
+    puts "Número insuficiente"
+ end
+
+ print "Cep: "
+ cep = gets.chomp.to_f
+
+ if cep > 0
+    puts "Incira o Cep."
+   elsif cep < 9
+    puts "Cep inesistente."
+ end
+
+ puts "Nome: #{nome}"
+ puts "Idade: #{idade}"
+ puts "Altura: #{altura}"
+ puts "Sexo: #{sexo}"
+ puts "Rua: #{rua}"
+ puts "Número: #{numero}"
