45eafaca5538b555722484d38d1b20bddbebb3f8
levi <levisantosluna@gmail.com>
2022-09-21 20:57:17 -0300

    Atualizando Números Inteiro Ângulos

--- Atividades/numero_inteiro_angulo.rb
+++ Atividades/numero_inteiro_angulo.rb
@@ -2,21 +2,22 @@
 
 
 print "digite um número de um ângulo: "
-x = gets.chomp.to_i
+x = gets.chomp.to_f
 
 v = x / 360 
 
-if v >= 0 && v <= 90
+if v >= 0.000 && v <= 0.090
   puts "Um quarto de volta"
- elsif v >= 90 && v <= 180
-  puts "Does quarto de volta"
- elsif v >= 180 && v <= 270
+ elsif v >= 0.090 && v <= 0.180
+  puts "Dois quarto de volta"
+ elsif v >= 0.180 && v <= 0.270
   puts "Trẽs quarto de volta"
- elsif v >= 270 && v <= 360
+ elsif v >= 0.270 && v <= 0.360
   puts "Quatro quarto de volta"
  else
   puts "#{v} volta(s)"
 end
+
 
 if x >= 0 && x <= 90
   puts "O ângolo #{x} está no primeiro quadrante"
