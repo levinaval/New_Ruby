cd0eb6cd08726e20884408ea867d70429cca4d11
levi <levisantosluna@gmail.com>
2022-09-20 22:41:20 -0300

    Desenvolvendo Números Inteiro Ângulos

--- Atividades/numero_inteiro_angulo.rb
+++ Atividades/numero_inteiro_angulo.rb
@@ -1,18 +1,20 @@
 #Escreva um programa que leia um número inteiro que corresponde a um ângulo e informe em qual quadrante este ângulo se encontra e quantas voltas ele dá (360 graus = uma volta).
 
 
-print "digite um número: "
+print "digite um número de um ângulo: "
 x = gets.chomp.to_i
 
-if x <= 0 && >= 90
-  puts "O #{x} está no primeiro quadrante"
- elsif x <= 90 && >= 180
-  puts "O #{x} está no segundo quadrante"
- elsif x <= 180 && >= 270
-  puts "O #{x} está no terceiro quadrante"
- elsif x <= 270 && >= 360
-  puts "O #{x} está no quarto quadrante"
+puts  x / 360 
+
+if x >= 0 && x <= 90
+  puts "O ângolo #{x} está no primeiro quadrante"
+ elsif x >= 90 && x <= 180
+  puts "O ângolo #{x} está no segundo quadrante"
+ elsif x >= 180 && x <= 270
+  puts "O ângolo #{x} está no terceiro quadrante"
+ elsif x >= 270 && x <= 360
+  puts "O ângolo #{x} está no quarto quadrante"
  else
-  puts "Esse grau não existe"
+  puts "Esse ângulo não existe"
  end
  
