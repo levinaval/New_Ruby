03acaa471cbeca2a41aa30cae278450aba6a04af
levi <levisantosluna@gmail.com>
2022-09-22 12:41:09 -0300

    Refatorando Número Inteiro Ângulo.rb

--- Atividades/numero_inteiro_angulo.rb
+++ Atividades/numero_inteiro_angulo.rb
@@ -2,30 +2,41 @@
 
 
 print "digite um número de um ângulo: "
-x = gets.chomp.to_f
+angulo_entrada = gets.to_i
 
-v = x / 360 
+angulo = angulo_entrada % 360
+voltas = angulo_entrada / 360
 
-if v >= 0.00 && v <= 0.90
-  puts "Um quarto de volta"
- elsif v >= 0.90 && v <= 0.179
-  puts "Dois quarto de volta"
- elsif v >= 0.180 && v <= 0.269
-  puts "Trẽs quarto de volta"
- elsif v >= 0.270 && v <= 0.359
-  puts "Quatro quarto de volta"
- elsif v >= 0.360
-  puts "#{v} volta(s)"
-end
+quadrante = 
+ if    angulo < 90  then "Primeiro"
+ elsif angulo < 180 then "Segundo"
+ elsif angulo < 270 then "Terceiro"
+ else                    "Quarto"  end
 
-if x >= 0 && x <= 89
-  puts "O ângolo #{x} está no primeiro quadrante"
- elsif x >= 90 && x <= 179
-  puts "O ângolo #{x} está no segundo quadrante"
- elsif x >= 180 && x <= 269
-  puts "O ângolo #{x} está no terceiro quadrante"
- elsif x >= 270 && x <= 359
-  puts "O ângolo #{x} está no quarto quadrante"
- 
- end
+  puts "O ângulo #{angulo_entrada} dá #{voltas} voltas e fica no #{quadrante} quadrante."
+
+#v = x / 360 
+#
+#if v >= 0.00 && v <= 0.90
+#  puts "Um quarto de volta"
+# elsif v >= 0.90 && v <= 0.179
+#  puts "Dois quarto de volta"
+# elsif v >= 0.180 && v <= 0.269
+#  puts "Trẽs quarto de volta"
+# elsif v >= 0.270 && v <= 0.359
+#  puts "Quatro quarto de volta"
+# elsif v >= 0.360
+#  puts "#{v} volta(s)"
+#end
+
+#if x >= 0 && x <= 89
+#  puts "O ângolo #{x} está no primeiro quadrante"
+# elsif x >= 90 && x <= 179
+#  puts "O ângolo #{x} está no segundo quadrante"
+# elsif x >= 180 && x <= 269
+#  puts "O ângolo #{x} está no terceiro quadrante"
+# elsif x >= 270 && x <= 359
+#  puts "O ângolo #{x} está no quarto quadrante"
+# 
+# end
  
