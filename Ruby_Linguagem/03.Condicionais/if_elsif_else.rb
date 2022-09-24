04cbbd522552b82999fd48596646bdb5d6c90e7a
levi <levisantosluna@gmail.com>
2022-09-23 23:28:26 -0300

    re fatorando if_elsif_else.rb

--- Ruby_Linguagem/03.Condicionais/if_elsif_else.rb
+++ Ruby_Linguagem/03.Condicionais/if_elsif_else.rb
@@ -1,15 +1,22 @@
-x = 50
-
-if x == 11
-  puts "X é igual a 10"
-
-  elsif x == 50
-    puts "É igual a 50"
-
- else
-  puts "X não é igual a 10"
-end
+#x = 50
+#
+#if x == 11
+#  puts "X é igual a 11"
+#
+#  elsif x == 50
+#    puts "É igual a 50"
+#
+# else
+#  puts "X não é igual a 10"
+#end
 
 # if- É uma comparação.(se for iqual...)
 # elsif- É uma comparação.(é outra coisa!)
 # else- É uma comparação(se não for isso é aquilo)
+
+x = 10
+if    x == 10 then "10"
+elsif x == 50 then "X é igual a 50"
+else               "X não é igual a 10" end
+
+puts "X é igual a #{x}"
