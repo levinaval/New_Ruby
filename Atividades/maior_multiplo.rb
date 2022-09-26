004f63d2032a54f9c288dc8f6954fa87e0c672ea
levi <levisantosluna@gmail.com>
2022-09-25 23:53:16 -0300

    Desenvolvendo maior_multiplo.rb

--- Atividades/maior_multiplo.rb
+++ Atividades/maior_multiplo.rb
@@ -1,1 +1,7 @@
 #Escreva um programa que leia dois números e informe se o maior é múltiplo do menor.
+
+print "Digite o Primeiro numero: "
+n1 = gets.to_i
+
+print "Digite o Segundo numero: "
+n1 = gets.to_i
