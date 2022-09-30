2e3a4f52dfdc72d622351d9cee61b3d181318ed6
levi <levisantosluna@gmail.com>
2022-09-30 00:03:40 -0300

    refatorando encontra quadrante.rb

--- Atividades/encontra_quadrante.rb
+++ Atividades/encontra_quadrante.rb
@@ -6,24 +6,16 @@ x = gets.chomp.to_i
 print "Digite um número para a Eixo Y: "
 y = gets.chomp.to_i
 
-if x > 0 && y > 0
-  puts "Primeiro quadrante"
- elsif x < 0 && y > 0
-  puts "Segundo quadrante"
- elsif x < 0 && y < 0
-  puts "Terceiro quadrante"
- elsif x > 0 && y < 0
-  puts "Quarto quadrante"
- elsif y = 0 && x > 0
-  puts "Eixo x positivo"
- elsif y = 0 && x < 0
-  puts "Eixo x negativo" 
- elsif x = 0 && y > 0
-  puts "Eixo y positivo"
- elsif x = 0 && y < 0
-  puts "Eixo y negativo"
- elsif x = 0 e y = 0
-  puts "Origem dos eixos"
- else
-  puts
-end
+quadrante = 
+if    x > 0 && y > 0 then  "Primeiro quadrante"
+elsif x < 0 && y > 0 then  "Segundo quadrante"
+elsif x < 0 && y < 0 then  "Terceiro quadrante"
+elsif x > 0 && y < 0 then  "Quarto quadrante"
+elsif y = 0 && x > 0 then  "Eixo x positivo"
+elsif y = 0 && x < 0 then  "Eixo x negativo" 
+elsif x = 0 && y > 0 then  "Eixo y positivo"
+elsif x = 0 && y < 0 then  "Eixo y negativo"
+elsif x = 0 && y = 0 then  "Origem dos eixos" end
+#else                             
+
+puts "O número digitado esta no #{quadrante}"
