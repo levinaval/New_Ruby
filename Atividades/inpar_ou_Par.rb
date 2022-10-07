6b9f7e46e72ce361fd4cb6227a33525e8222db04
levi <levisantosluna@gmail.com>
2022-10-06 23:43:36 -0300

    ingluindo jogadores ao Inpar ou Par.rb

--- Atividades/inpar_ou_Par.rb
+++ Atividades/inpar_ou_Par.rb
@@ -1,1 +1,8 @@
 #Brincadeira do Impa ou par: escreva um programa que leia dois nomes e dois valores inteiros, que correspondem ao que cada um colocou, e informe quem ganhou o “ímpar ou par”. 
+
+print "Nome do primeiro jogador: ",""
+jogador1 = gets.chomp
+
+
+print "Nome do segundo jogador: ",""
+jogador2 = gets.chomp
