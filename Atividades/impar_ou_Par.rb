03cf3abd3edc02141a91940f4886141ad13c1ca4
levi <levisantosluna@gmail.com>
2022-10-10 23:11:19 -0300

    Re-organizando Impar ou Par.rb

--- Atividades/impar_ou_Par.rb
+++ Atividades/impar_ou_Par.rb
@@ -3,20 +3,20 @@
 print "Nome do jogador PAR: ",""
 jogador1 = gets.chomp
 
-print "Jogador PAR escolha um número: ",""
-n1 = gets.chomp.to_i
-
 print "Nome do jogador IMPAR: ",""
 jogador2 = gets.chomp
+
+print "Jogador PAR escolha um número: ",""
+n1 = gets.chomp.to_i
 
 print "Jogador IMPAR escolha um número: ",""
 n2 = gets.chomp.to_i
 
 resultado = if (n1 + n2) %2 == 0 then "Pra" else "Impar" end 
- if resultado == "Par" then 
-   
- elsif 
-   
- end
+ #if resultado == "Par" then 
+ #  
+ #elsif 
+ #  
+ #end
 
 puts "O vencedor é #{resultado}"
