d4f7b39f235ec6115c92461ad09afa9574f5af71
levi <levisantosluna@gmail.com>
2022-10-15 23:48:44 -0300

    player 1

--- Atividades/impar_ou_Par.rb
+++ Atividades/impar_ou_Par.rb
@@ -1,12 +1,12 @@
 #Brincadeira do Impa ou par: escreva um programa que leia dois nomes e dois valores inteiros, que correspondem ao que cada um colocou, e informe quem ganhou o “ímpar ou par”. 
 
 print "Nome do jogador PAR: ",""
-jogador1 = gets.chomp
+player1 = gets.chomp
 
 print "Nome do jogador IMPAR: ",""
 jogador2 = gets.chomp
 
-print "Jogador PAR #{jogador1} escolha um número: ",""
+print "Jogador PAR #{player1} escolha um número: ",""
 n1 = gets.chomp.to_i
 
 print "Jogador IMPAR #{jogador2} escolha um número: ",""
@@ -14,7 +14,7 @@ n2 = gets.chomp.to_i
 
 resultado = if (n1 + n2) %2 == 0 then "Pra" else "Impar" end 
  
-vencedor = if resultado == "Par" then jogador1 else jogador2 end
+vencedor = if resultado == "Par" then player1 else jogador2 end
 
 ns = if vencedor == "Par" then ni else n2 end
    
