958bda4f5e3eb0d9a8b804892e31811429db1f0a
levi <levisantosluna@gmail.com>
2022-10-15 23:49:12 -0300

    player 2

--- Atividades/impar_ou_Par.rb
+++ Atividades/impar_ou_Par.rb
@@ -4,17 +4,17 @@ print "Nome do jogador PAR: ",""
 player1 = gets.chomp
 
 print "Nome do jogador IMPAR: ",""
-jogador2 = gets.chomp
+player2 = gets.chomp
 
 print "Jogador PAR #{player1} escolha um número: ",""
 n1 = gets.chomp.to_i
 
-print "Jogador IMPAR #{jogador2} escolha um número: ",""
+print "Jogador IMPAR #{player2} escolha um número: ",""
 n2 = gets.chomp.to_i
 
 resultado = if (n1 + n2) %2 == 0 then "Pra" else "Impar" end 
  
-vencedor = if resultado == "Par" then player1 else jogador2 end
+vencedor = if resultado == "Par" then player1 else player2 end
 
 ns = if vencedor == "Par" then ni else n2 end
    
