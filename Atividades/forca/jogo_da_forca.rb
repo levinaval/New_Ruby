48f7c43a296e355416adb39e08707e53662e8d04
levi <levisantosluna@gmail.com>
2022-10-17 22:31:40 -0300

    Criando método para boas vindas

--- Atividades/forca/jogo_da_forca.rb
+++ Atividades/forca/jogo_da_forca.rb
@@ -1,17 +1,23 @@
-def jogo_da_forca
-  nome = da_boas_vindas
-  
-  loop do
-      joga nome
-      if nao_quer_jogar?
-          break
-      end
-  end
- end 
+ #def jogo_da_forca
+ # nome = da_boas_vindas
+ # 
+ # loop do
+ #     joga nome
+ #     if nao_quer_jogar?
+ #         break
+ #     end
+ # end
+ #end 
 
  #desenvolvendo
  def jogo_da_forca(da_boas_vindas)
-    puts "Bem vindos"
-  end 
+
+    puts "Sejão #{da_boas_vindas}, desejão jogar?"
+
+ end 
   
-   jogo_da_forca(da_boas_vindas)
+ jogo_da_forca("Bem vindos")
+
+  joga = gets.chomp
+
+  
