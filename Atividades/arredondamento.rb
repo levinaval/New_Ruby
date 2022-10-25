70903acc800b5b3e0aab9ad7f9c1fd50d0357c21
levi <levisantosluna@gmail.com>
2022-10-24 23:30:47 -0300

    Avançando Arredondamento.rb

--- Atividades/arredondamento.rb
+++ Atividades/arredondamento.rb
@@ -1,1 +1,3 @@
 #Escreva um programa que leia um número real e faça o arredondamento para inteiro. Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro. 
+
+print "Escreva um Número real: "
