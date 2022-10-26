3510d82587667010622c37ff607960ac265b1d4e
levi <levisantosluna@gmail.com>
2022-10-25 22:44:14 -0300

    Acresentando um gets Arredondamento.rb

--- Atividades/arredondamento.rb
+++ Atividades/arredondamento.rb
@@ -1,3 +1,5 @@
 #Escreva um programa que leia um número real e faça o arredondamento para inteiro. Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro. 
 
 print "Escreva um Número real: "
+n_real = gets.chomp.to_f
+
