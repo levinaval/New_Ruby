4e090641e4fc9ef2254a587b3066c74df12f4237
levi <levisantosluna@gmail.com>
2022-10-26 23:12:47 -0300

    Transcreendo para o inglês Arredondamento.rb

--- Atividades/arredondamento.rb
+++ Atividades/arredondamento.rb
@@ -1,10 +1,10 @@
 #Escreva um programa que leia um número real e faça o arredondamento para inteiro. Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro. 
 
 print "Escreva um Número real: "
-n_real = gets.chomp.to_f
+number_real = gets.chomp.to_f
 
-if n_real >= 0.5
-  puts n_real + 1
-elsif n_real == 1
-  puts n_real
+if number_real >= 0.5
+  puts number_real + 1
+elsif number_real == 1
+  puts number_real
 end
