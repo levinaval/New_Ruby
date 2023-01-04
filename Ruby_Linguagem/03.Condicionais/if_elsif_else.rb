8adf5140dd4ef69bf8ab05099e2720e985b5720a
levi <levisantosluna@gmail.com>
2023-01-03 23:54:34 -0300

    first commit

--- Ruby_Linguagem/03.Condicionais/if_elsif_else.rb
+++ Ruby_Linguagem/03.Condicionais/if_elsif_else.rb
@@ -21,4 +21,4 @@ if    x == 10 then "X é igual a 10"
 elsif x == 50 then "X é igual a 50"
 else               "X não é igual nem a 10 e nem 50" end
 
-puts "Este valor #{x} #{comparacao}"
+puts "Este valor #{x} é  #{comparacao}"
