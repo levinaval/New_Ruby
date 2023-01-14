730ed437dfe7ec724f8bbd58fdd146a7de691859
levi <levisantosluna@gmail.com>
2023-01-14 19:08:46 -0300

    Treinado

---
+++ Ruby_Linguagem/03.Condicionais/.rb
@@ -0,0 +1,8 @@
+x = 10
+
+resultado = 
+if    x == 10 then "O resultado está correto!"
+elsif x == 11 then "O resultado é diferente mas ainda tolerante."
+else               "O resultado está muito errado!" end
+  
+  puts "Em comparação #{resultado}"
