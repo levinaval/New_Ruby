2d284dc94ce54dd1e421668296fc966e2a09261c
levi <levisantosluna@gmail.com>
2023-01-27 22:33:43 -0300

    Criando nomes iguais.rb

---
+++ Atividades/nomes_iguais.rb
@@ -0,0 +1,17 @@
+#15. Escreva um programa que leia dois nomes e informe se os mesmos são iguais. Além de informar se os nomes são iguais, ele deve informar se há diferenças na digitação de letras maiúsculas/ minúsculas. DICA: use o método upcase de String. 
+
+
+print "Escreva o primeiro nome: "
+p1 = gets.chomp
+
+print "Escreva o segundo nome: "
+p2 = gets.chomp
+
+comparacao =
+if p1 == p2 then "Os nomes são iguais"
+  
+elsif p1 == p2 then "Os nomes são iguais"
+  
+else
+  
+end
