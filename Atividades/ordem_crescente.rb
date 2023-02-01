bb541212c95927656d042124640bf750d5cc8cc2
levi <levisantosluna@gmail.com>
2023-01-31 22:43:54 -0300

    Primeros passos ordem crescente.rb

--- Atividades/ordem_crescente.rb
+++ Atividades/ordem_crescente.rb
@@ -1,1 +1,11 @@
 #Escreva um programa que leia 3 números inteiros e mostre-os em ordem crescente.
+
+puts "Digite núremos em ordem aleatoria:"
+print "Primeiro número: "
+n1 = gets.chomp.to_i
+
+print "Segundo número: "
+n2 = gets.chomp.to_i
+
+print "Terceiro número: "
+n3 = gets.chomp.to_i
