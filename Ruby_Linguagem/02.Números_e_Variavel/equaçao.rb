bc096285a2350e9361734b57ddb8ea2decfd80ea
levi <levisantosluna@gmail.com>
2023-03-03 22:38:04 -0300

    Valor de delta Equação

--- Ruby_Linguagem/02.Números_e_Variavel/equaçao.rb
+++ Ruby_Linguagem/02.Números_e_Variavel/equaçao.rb
@@ -1,8 +1,12 @@
-print "Digite o valor de x²: "
+print "Digite o valor de A que é o coeficiente de x²: "
 a = gets.chomp.to_i
 
-print "Digite o valor de x: "
+print "Digite o valor de B que é o coeficiente de x: "
 b = gets.chomp.to_i
 
-print "Digite o ultimo valor: "
+print "Digite o valor C que é o ultimo coeficiente: "
 c = gets.chomp.to_i
+
+delta = (b * b) - 4 * a * c
+
+puts "O valor de delta é #{delta}"
