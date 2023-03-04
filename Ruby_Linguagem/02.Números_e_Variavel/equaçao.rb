17a8e67e6d8168d2b69c586dd15e979aed95e617
levi <levisantosluna@gmail.com>
2023-03-04 00:49:21 -0300

    Valor de x1 e x2 Equação

--- Ruby_Linguagem/02.Números_e_Variavel/equaçao.rb
+++ Ruby_Linguagem/02.Números_e_Variavel/equaçao.rb
@@ -10,3 +10,9 @@ c = gets.chomp.to_i
 delta = (b * b) - 4 * a * c
 
 puts "O valor de delta é #{delta}"
+
+x1 = -(b) + (3 / delta) / 2 * a
+x2 = -(b) - (3 / delta) / 2 * a
+
+puts "O valor de x1 é #{x1}"
+puts "O valor de x2 é #{x2}"
