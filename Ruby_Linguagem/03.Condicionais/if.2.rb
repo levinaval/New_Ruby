166038004b161a97d8d699b5617c01e17826dcec
levi <levisantosluna@gmail.com>
2023-03-20 21:20:03 -0300

    Refatorando if.2.rb

--- Ruby_Linguagem/03.Condicionais/if.2.rb
+++ Ruby_Linguagem/03.Condicionais/if.2.rb
@@ -1,8 +1,6 @@
 x = 11
 
 resultado = 
-if    x == 10 then "o resultado está correto!"
-elsif x == 11 then "o resultado é diferente mas ainda tolerante."
-else               "o resultado está muito errado!" end
+if x == 10 then "o resultado está correto!" elsif x == 11 then "o resultado é diferente mas ainda tolerante." else "o resultado está muito errado!" end
   
-  puts "Em comparação #{resultado}"
+puts "Em comparação #{resultado}"
