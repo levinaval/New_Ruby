4da498625d4cf9312bb9333e6b6bb408c33543fc
levi <levisantosluna@gmail.com>
2023-03-20 21:35:53 -0300

    Desenvolvendo case.rb

--- Ruby_Linguagem/03.Condicionais/case.rb
+++ Ruby_Linguagem/03.Condicionais/case.rb
@@ -4,13 +4,15 @@ mes = gets.chomp
 case mes
 
 when "janeiro"
-  puts "Tem 31 dias"
+  puts "Tem 31 dias, no dia 1 é o ano novo!"
 when "fevereiro"
-  puts "Tem 28 dias"
+  puts "Tem 28 dias e tem o carnaval de 17 a 22!"
 when "março"
-  puts "Tem 30 dias"
+  puts "Tem 30 dias, não tem feriado."
 when "abril"
-  puts "tem 30 dias"
+  puts "tem 30 dias, no dia 1 é o dia da mentira, dia 7 é a paixão de cristo, dia 9 domingo de pascoa, dia 21 tiradentes."
+when "maio"
+  puts "tem 31 dias, no dia 1 é dia do trabalho, 14 é dia das mães."
 else
   puts "Isso eu não sei!!"
 end 
