e447bb8f1e2d04c9730f7c66aaeb61644aebfc72
levi <levisantosluna@gmail.com>
2023-06-05 23:03:11 -0300

    Construindo Números e matemática

---
+++ Try_Ruby/numero_matematicos.rb
@@ -0,0 +1,19 @@
+class Matematica
+  def mutiplicar
+    4 * 10
+  end
+end
+
+mat = Matematica.new
+
+
+class Matematica
+  def subtrair
+    12 - 5
+  end
+end
+
+mat = Matematica.new
+
+puts mat.mutiplicar
+puts mat.subtrair
