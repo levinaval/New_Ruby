8ebf23ab0f9de84c17e00eb2087050529fc97736
levi <levisantosluna@gmail.com>
2023-06-06 23:29:05 -0300

    Refatorando contagem.br

---
+++ Try_Ruby/03.contagem.rb
@@ -0,0 +1,11 @@
+class Compras
+  def lista_de_compas
+
+    lista_de_compras = ["Feijão Preto", "Feijão arioca", "Arroz Parbolizado", "Macarão", "Cuscuz", "Frinha"]
+    
+  end  
+end
+
+compras = Compras.new
+
+puts compras.lista_de_compas.length + 1
