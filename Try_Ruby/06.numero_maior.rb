827ac2e3b19f1ac315091ed0911e6f3b0e21e2c5
levi <levisantosluna@gmail.com>
2023-06-12 21:58:04 -0300

    Refatoração de 06 número maior rb

---
+++ Try_Ruby/06.numero_maior.rb
@@ -0,0 +1,16 @@
+class Maior
+  def lista_1
+    lista = [34, 55, 90]
+    
+  end
+  
+  def lista_2
+    grupo = [12, 50, 100]
+    
+  end
+end
+
+m = Maior.new
+
+puts m.lista_1.max
+puts m.lista_2.max
--- Try_Ruby/numero_maior.rb
+++
@@ -1,8 +0,0 @@
-
-def numero_mair
-
-  numero_mair = [34, 55, 90]
-  
-end
-
-puts numero_mair.max
