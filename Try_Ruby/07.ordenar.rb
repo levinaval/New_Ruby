f017fd85a3789d5c58777048945cab36a4b28ed4
levi <levisantosluna@gmail.com>
2023-06-12 22:10:03 -0300

    Refatoração de 07 ordenar rb

---
+++ Try_Ruby/07.ordenar.rb
@@ -0,0 +1,10 @@
+class Ordenar
+  def loteria
+   bilhete = [54, 23, 14, 1, 3, 10]
+    
+  end  
+end
+
+ord = Ordenar.new
+
+print ord.loteria.sort
--- Try_Ruby/ordenar.rb
+++
@@ -1,7 +0,0 @@
-def loteria
-
-  loteria = [54, 23, 14, 1, 3, 10]
-  
-end
-
-print loteria.sort
