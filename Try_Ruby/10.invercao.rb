1978844537bf4d9321eeb32983245e7d1255c327
levi <levisantosluna@gmail.com>
2023-06-15 23:42:44 -0300

    inverção rb

---
+++ Try_Ruby/10.invercao.rb
@@ -0,0 +1,11 @@
+# Comando para efetuar inversão entre a primeira e a última linha do texto.
+
+torrada = "Minha torrada voou da minha mão
+  E minha torrada se foi para a lua.
+  Mas quando eu vejo isso na televisão,
+  Colocando nossa bandeira no cometa Halley,
+  Mais ainda eu quero come-la.
+  
+  Isto rima ?"
+
+puts torrada.lines.reverse
