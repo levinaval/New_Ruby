6da172db84aaaaf81a48982d7423eac51c4aea19
levi <levisantosluna@gmail.com>
2023-06-15 23:24:49 -0300

    Poema rb

---
+++ Try_Ruby/09.poemas.rb
@@ -0,0 +1,17 @@
+def poema
+  torrada = ["Minha torrada voou da minha mão
+  E minha torrada se foi para a lua.
+  Mas quando eu vejo isso na televisão,
+  Colocando nossa bandeira no cometa Halley,
+  Mais ainda eu quero come-la.
+  
+  Isto rima ?"]    
+
+  manga = ["Minha manga voou da minha mão
+  E minha manga se foi para a lua.
+  Mas quando eu vejo isso na televisão,
+  Colocando nossa bandeira no cometa Halley,
+  Mais ainda eu quero come-la."]
+end  
+
+  puts poema.gsub("torrada", "manga")
