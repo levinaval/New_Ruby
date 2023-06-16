98eef1df526f3c0d05ed6212cb98e679eb99c66a
levi <levisantosluna@gmail.com>
2023-06-15 23:52:31 -0300

    Colocando class em poema rb

--- Try_Ruby/09.poemas.rb
+++ Try_Ruby/09.poemas.rb
@@ -1,19 +1,22 @@
 # O método gsub é abreviação para substituição global. Ele substitui todas as ocorrências de “torrada” para “manga”.
 
-def poema
-  torrada = "Minha torrada voou da minha mão
-  E minha torrada se foi para a lua.
-  Mas quando eu vejo isso na televisão,
-  Colocando nossa bandeira no cometa Halley,
-  Mais ainda eu quero come-la.
+class Substituicao
+  def poema
+    torrada = "Minha torrada voou da minha mão
+    E minha torrada se foi para a lua.
+    Mas quando eu vejo isso na televisão,
+    Colocando nossa bandeira no cometa Halley,
+    Mais ainda eu quero come-la.
+    
+    Isto rima ?"    
   
-  Isto rima ?"    
+    manga = "Minha manga voou da minha mão
+    E minha manga se foi para a lua.
+    Mas quando eu vejo isso na televisão,
+    Colocando nossa bandeira no cometa Halley,
+    Mais ainda eu quero come-la."
+  end    
+end
 
-  manga = "Minha manga voou da minha mão
-  E minha manga se foi para a lua.
-  Mas quando eu vejo isso na televisão,
-  Colocando nossa bandeira no cometa Halley,
-  Mais ainda eu quero come-la."
-end  
-
-  puts poema.gsub("manga", "torrada")
+subt = Substituicao.new 
+puts subt.poema.gsub("torrada", "manga")
