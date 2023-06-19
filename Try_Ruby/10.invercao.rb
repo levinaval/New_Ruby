861dcd8d4a224161a2b2245aa7a6f03e0ae1ac01
levi <levisantosluna@gmail.com>
2023-06-18 23:53:42 -0300

    class em inverção rb

--- Try_Ruby/10.invercao.rb
+++ Try_Ruby/10.invercao.rb
@@ -4,14 +4,20 @@
 
 # Este comando efetuar inversão entre a primeira e a última linha do texto.
 
-torrada = "Minha torrada voou da minha mão
-  E minha torrada se foi para a lua.
-  Mas quando eu vejo isso na televisão,
-  Colocando nossa bandeira no cometa Halley,
-  Mais ainda eu quero come-la.
-  
-  Isto rima ?"
+class Manipulacao
+  def string
+    torrada = "Minha torrada voou da minha mão
+    E minha torrada se foi para a lua.
+    Mas quando eu vejo isso na televisão,
+    Colocando nossa bandeira no cometa Halley,
+    Mais ainda eu quero come-la.
+    
+    Isto rima ?"  
+  end
+end
 
-puts torrada.swapcase#lines.reverse
+mani = Manipulacao.new
 
-poema.downcase ou poema.swapcase
+puts mani.string.swapcase.lines.reverse
+puts mani.string.downcase.lines.reverse 
+puts mani.string.swapcase.lines.reverse
