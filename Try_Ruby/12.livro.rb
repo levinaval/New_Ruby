f7fc6695336cd802d8dbf009c5f9d347b13afa6f
levi <levisantosluna@gmail.com>
2023-06-26 23:15:44 -0300

    Refatorando livro.rb

--- Try_Ruby/12.livro.rb
+++ Try_Ruby/12.livro.rb
@@ -1,8 +1,11 @@
+livros = {
+  "O Arco-Íris da Gravidade" => :esplendido_uma_obra_prima,
+  "Outro Livro" => :maravilhoso,
+  "Mais um Livro" => :fantastico
+}
+
 print "Escreva um título de livro: "
 titulo = gets.chomp
 
-livros = {}
-livros["O Arco-Íris da Gravidade"] = :esplendido
-
 puts
-puts "Este livro é #{livros[titulo]}"
+puts "Este livro é #{livros[titulo]}" if livros.key?(titulo)
