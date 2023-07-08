99b4d664a61878a1ed91eefe16f64278f9970799
levi <levisantosluna@gmail.com>
2023-07-07 21:43:24 -0300

    Adicionado comentários a 13classi(harsh)

--- Try_Ruby/13.classi.(Harsh).rb
+++ Try_Ruby/13.classi.(Harsh).rb
@@ -9,6 +9,8 @@ end
 livro = Livro.new
 titulo = livro.pedir_titulo
 
+# Esta hash é criado com um valor padrão de 0. Esse hash será usado para contar o número de ocorrências de cada classificação.
+
 classificacoes = Hash.new(0)
 
 livros = {
@@ -17,8 +19,14 @@ livros = {
   "Dom Quixote" => "Classificação 1"
 }
 
+# O método values é chamado no hash livros para obter uma lista dos valores das classificações.
+
+#Utilizando o método each, cada valor da lista de classificações é iterado. Dentro do loop, a contagem da classificação é incrementada no hash classificacoes.
+
 livros.values.each do |classificacao|
   classificacoes[classificacao] += 1
 end
 
 puts classificacoes
+
+# Em resumo, esse código solicita um título de livro ao usuário, conta quantos livros existem em cada classificação predefinida e exibe o resultado dessa contagem. Isso pode ser útil para obter estatísticas sobre as classificações dos livros fornecidos.
