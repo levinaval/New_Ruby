cf371bad664878a5ba20995707ca18a283bf4568
levi <levisantosluna@gmail.com>
2023-07-18 23:14:50 -0300

    Class Bloco

---
+++ Try_Ruby/14.bloco.rb
@@ -0,0 +1,11 @@
+# basicamente, um bloco é um pedaço de código Ruby cercado por chaves {}.
+
+class Repetir
+  def times
+    5.times{puts "Levi"}
+  end
+end
+
+repetir = Repetir.new
+
+repetir.times
