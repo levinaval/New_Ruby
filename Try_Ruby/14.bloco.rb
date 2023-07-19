233788686d8c2cfaa3797b09d22d03a2219c1afe
levi <levisantosluna@gmail.com>
2023-07-18 23:15:47 -0300

    Pequena correção em Class Bloco

--- Try_Ruby/14.bloco.rb
+++ Try_Ruby/14.bloco.rb
@@ -1,11 +1,11 @@
 # basicamente, um bloco é um pedaço de código Ruby cercado por chaves {}.
 
 class Repetir
-  def times
+  def tempo
     5.times{puts "Levi"}
   end
 end
 
 repetir = Repetir.new
 
-repetir.times
+repetir.tempo
