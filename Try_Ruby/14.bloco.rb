db215599a371c6a5dc5193da25d64ba3e2b62607
levi <levisantosluna@gmail.com>
2023-07-18 23:16:58 -0300

    Incluindo um comentario em Bloco

--- Try_Ruby/14.bloco.rb
+++ Try_Ruby/14.bloco.rb
@@ -1,5 +1,7 @@
 # basicamente, um bloco é um pedaço de código Ruby cercado por chaves {}.
 
+# Blocos são sempre associados a métodos. Como o método times, que pega o bloco e roda o código dele várias e várias vezes. (No nosso exemplo: cinco vezes).
+
 class Repetir
   def tempo
     5.times{puts "Levi"}
