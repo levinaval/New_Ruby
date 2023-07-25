75566b7ab084227ec88514b41ecf8ecb373b521a
levi <levisantosluna@gmail.com>
2023-07-24 22:13:40 -0300

    Criando class Player em about extra credit

--- koans/about_extra_credit.rb
+++ koans/about_extra_credit.rb
@@ -18,3 +18,46 @@ class DiceSet
     @values = Array.new(num_rolls) { rand(1..6) }
   end
 end
+
+class player
+  attr_reader :name, :score
+
+  def initialize
+  @name, @score, @score_this_turn, @turn_in_progress = name, 0, 0, false  
+  end
+
+  def roll_dice(dice_set)
+    dice_set.roll(5)
+    values = dice_set.values
+
+    if score_this_turn(values) > 0
+      @score_this_turn += score_this_turn(values)
+      @turn_in_progress = true
+    else
+      end_turn
+    end
+
+    values
+  end
+
+  def score_this_turn(values)
+    score = 0
+
+    count_of_values.each do |value, count|
+      if count >= 3
+        score += (value == 1 ? 1000 : value * 100)
+        count -= 3
+      end
+
+      score += count * 100 if value == 1
+      score += count * 50 if value == 5
+    end
+
+    score
+  end
+
+  def end_turn
+    @score += @score_this_turn if @turn_in_progress 
+    @score_this_turn, @turn_in_progress = 0, false
+  end
+end
