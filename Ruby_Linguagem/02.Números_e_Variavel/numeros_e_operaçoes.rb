f1590d97ba213166c590f0ef424e04ca8d96cf1e
levi <levisantosluna@gmail.com>
2023-11-18 09:02:48 -0300

    Atualizando

--- Ruby_Linguagem/02.Números_e_Variavel/numeros_e_operaçoes.rb
+++ Ruby_Linguagem/02.Números_e_Variavel/numeros_e_operaçoes.rb
@@ -3,15 +3,18 @@
 
 # ou
 
-x = 5
+x = 123
 y = 10
 
 soma = x + y
 subt = x - y
 mult = x * y
 div = x / y
+modulo = x % y
 
 puts soma
 puts subt
 puts mult
 puts div
+puts modulo 
+# módulo é útil em muitos cenários, como para determinar se um número é par ou ímpar, ou para criar ciclos repetitivos em um intervalo específico.
