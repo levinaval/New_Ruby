4f616de99238c8ebe0029af94acfed773ee37fab
levi <levisantosluna@gmail.com>
2023-11-23 17:17:26 -0300

    Criando pessoas da familia

---
+++ Poo/assesores_de_atributos/attr_reader/pessoa_da_familia.rb
@@ -0,0 +1,33 @@
+
+#class PessoasDaFamilia
+#  attr_reader :nome, :altura 
+
+#  def initialize(nome, altura)
+#    @nome = nome
+#    @altura = altura
+#  end
+#end
+
+#individo = PessoasDaFamilia.new("Alfrede", 1.75)
+
+#puts individo.nome
+#puts individo.altura
+
+# or  
+
+class PessoasDaFamilia
+  attr_reader :nome, :altura 
+
+  def initialize(nome, altura)
+    @nome = nome
+    @altura = altura
+  end
+
+  def pessoa
+    puts "Eu me chamo #{self.nome}, sou o mais alto da familia com #{@altura} metros!"
+  end
+end
+
+individo = PessoasDaFamilia.new("Alfrede", 1.75)
+
+individo.pessoa
