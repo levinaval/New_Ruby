774d57bbf298a7846c24825af96719f612039727
levi <levisantosluna@gmail.com>
2023-11-26 21:27:17 -0300

    Colocando todos os arquivos em lanchonete

--- Poo/composição/lanchonete/main/order_finished.rb
+++
@@ -1,9 +0,0 @@
-class OrderFinished
-  def initialize(snack_order, drink_order, order_inclusion)
-    @snack_order, @drink_order, @order_inclusion = snack_order, drink_order, order_inclusion
-  end
-
-  def last_car
-    puts "Pedido finalisado #{@nack_order}, #{@drink_order} e #{@order_inclusion}"
-  end
-end
