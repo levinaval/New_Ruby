f3966e2b9bea33777dc84f9858094b67c5658a6c
levi <levisantosluna@gmail.com>
2024-02-26 10:57:45 -0300

    Alterando operaÇões Aritméticas

--- Ruby_Linguagem/10.Métodos/operacoes_aritmetica.rb
+++ Ruby_Linguagem/10.Métodos/operacoes_aritmetica.rb
@@ -1,30 +1,30 @@
 puts "Qual operação atritmética deseja fazer?"
 print "1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão, 5 - Exponenciação, 6 - Porcentagem: "
 
-escolha = gets.chomp
+escolha = gets.chomp.to_i
 
 puts "Escreva dois Números."
 print "O primeiro: "
 n1 = gets.chomp.to_f
 
 print "O segundo: "
-n2 = gets.chomp.to_i
+n2 = gets.chomp.to_f
 
 def operacao(escolha, n1, n2)
-  case escolha
 
+case escolha
   when  1
      puts n1 + n2   
-   whem  2
+  when  2
      puts n1 - n2
-   whem  3
+  when  3
      puts n1 * n2
-   whem  4
+  when  4
      puts n1 / n2
-   whem  5
+  when  5
      puts n1 ** n2
-   whem  6
-     puts n1 % n2
+  when  6
+     puts (n1 * n2) / 100
    else
      puts "Não tem esta opção"
      return
