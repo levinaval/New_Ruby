0c459ae34eb1f466eccd79b9da05f6be67c9e51d
levi <levisantosluna@gmail.com>
2024-03-26 23:46:42 -0300

    Chamando todas as Fuções

--- Ruby_Linguagem/10.Métodos/funcoes_aritmeticas.rb
+++ Ruby_Linguagem/10.Métodos/funcoes_aritmeticas.rb
@@ -37,3 +37,12 @@ end
  def tangente_radiano(an)
    return Math.tan(an)
  end
+
+ puts "Valor absoluto de -5: #{valor_absoluto(-5)}"
+ puts "Exponenciação de 2 elevado a 3: #{exponenciacao(2, 3)}"
+ puts "Valor inteiro de 3.14: #{valor_inteiro(3.14)}"
+ puts "Raiz quadrada de 16: #{raiz(16)}"
+ puts "Valor de Pi: #{retorna_pi()}"
+ puts "Seno de 90 graus em radiano: #{seno_radianos(Math::PI / 2)}"
+ puts "Cosseno de 0 graus em radianos: #{cosseno_radianos(0)}"
+ puts "Tangente de 45 graus em radianos: #{tangente_radianos(Math::PI / 4)}"
