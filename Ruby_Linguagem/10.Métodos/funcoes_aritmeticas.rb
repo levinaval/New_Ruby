5048a5e29e62546c8f82507b994bf5911714a64c
levi <levisantosluna@gmail.com>
2024-03-26 23:23:06 -0300

    Criando funções para Valor Absoluto, Exponenciação, Valor Inteiro e Raiz quadrada

--- Ruby_Linguagem/10.Métodos/funcoes_aritmeticas.rb
+++ Ruby_Linguagem/10.Métodos/funcoes_aritmeticas.rb
@@ -1,5 +1,21 @@
-def raiz(n)
-  Math.sqrt(n)
+#Função para calcular o valor de um número.
+def valor_absoluto(n)
+  return n.abs
 end
 
-puts raiz(25)
+#Função para calcular a exponenciação de um elevado a uma potência.
+def exponenciacao(base, expoente)
+  return base ** expoente
+end
+
+#Função para calcular o valor inteiro de um número.
+def valor_inteiro(n)
+  return n.to_i
+end
+
+#Função para calcular a raiz quadrada de um número.
+def raiz(n)
+  return Math.sqrt(n)
+end
+
+ 
