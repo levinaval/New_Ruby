90037f14fe1941348622d5f0afbe77ea68c7e610
levi <levisantosluna@gmail.com>
2024-03-29 19:15:03 -0300

    Criando mais compareções em  Op_logicos

--- logica/op_logicos.rb
+++ logica/op_logicos.rb
@@ -1,9 +1,33 @@
 a = 2
 b = 3
-c = 5
+c = 6
 
-def comparacao(a, b)
+def maior(a, b)
   return (a > b)
 end
 
+def menor(a, b)
+  return (a < b)
+end
+
+def comparacao(a, b)
+  return (a == b)
+end
+
+def diferente(a, b)
+  return (a != b)
+end
+
+def maior_igual(a,b)
+  return (a >= b)
+end
+
+def superior(a, b, c)
+  return (c >= a + b)
+end
+puts maior(a, b)
+puts menor(a, b)
 puts comparacao(a, b)
+puts diferente(a, b)
+puts maior_igual(a, b)
+puts superior(a, b, c)
