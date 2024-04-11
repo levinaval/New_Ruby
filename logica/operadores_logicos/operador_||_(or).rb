0d089d83886fc19afe320cfd0fe87cbe4f33b402
levi <levisantosluna@gmail.com>
2024-04-10 23:43:25 -0300

    Criando arquevo operador_||_(or).rb

---
+++ logica/operadores_logicos/operador_||_(or).rb
@@ -0,0 +1,8 @@
+# Retorna verdadeiro se pelomenos uma das expressões for verdadeiro.
+
+x = 5
+y = 10
+
+puts (x > 0) || (y < 15) # Saída: true
+puts (x < 0) || (y < 15) # Saída: true
+puts (x < 0) || (y > 15) # Saída: false
