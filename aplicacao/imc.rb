54523e84bc09d81afacd263e17a946ccafd18e59
levi <levisantosluna@gmail.com>
2024-04-27 00:41:35 -0300

    Concluindo IMC.rb

--- aplicacao/imc.rb
+++ aplicacao/imc.rb
@@ -17,3 +17,18 @@ class CalculadoraIMC
   end
 end
 
+puts "Calculadora de IMC"
+print "Por favor, insira seu peso em Kg: "
+peso = gets.chomp.to_f
+
+print "Por favor, insira sua altura em metros: "
+altura = gets.chomp.to_f
+
+calculadora = CalculadoraIMC.new
+
+imc = calculadora.calcular_imc(peso, altura)
+
+interpretacao = calculadora.interpretar_imc(imc)
+
+puts "/n Seu IMC é: #{imc}"
+puts "Interpretação: #{interpretacao}"
