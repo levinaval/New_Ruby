66053c16e02831dcbea8ea27f191b307cfaa135e
levi <levisantosluna@gmail.com>
2024-04-28 15:28:45 -0300

    Alterando detalhes de App idiomas.rb

--- aplicacao/app_idiomas.rb
+++ aplicacao/app_idiomas.rb
@@ -1,10 +1,11 @@
 class AppIdiomas
   attr_accessor :vocabulario
+
   def initialize
     @vocabulario = {}
   end
 
-  def adicionar_palavras(palavra, traducao)
+  def adicionar_palavra(palavra, traducao)
     @vocabulario[palavra] = traducao
   end
 
@@ -15,12 +16,39 @@ end
 
 idiomas = AppIdiomas.new
 
-idiomas.adicionar_palavras("hello", "ola")
-idiomas.adicionar_palavras("goodbye", "adeus")
-idiomas.adicionar_palavras("cat", "gato")
-idiomas.adicionar_palavras("dog", "cachorro")
+loop do
+  puts "Bem-vindo ao programa de aprendizado de idiomas!"
+  puts "O que você gostaria de fazer?"
+  puts "1. Adicionar palavra ao vocabulário"
+  puts "2. Traduzir uma palavra"
+  puts "3. Sair"
 
-puts idiomas.traduzir("hello")
-puts idiomas.traduzir("goodbye")
-puts idiomas.traduzir("cat")
-puts idiomas.traduzir("dog")
+  print "Escolha uma opção: "
+  opcao = gets.chomp.to_i
+
+  case opcao
+  when 1
+    print "Digite a palavra em inglês: "
+    palavra = gets.chomp
+    print "Digite a tradução em português: "
+    traducao = gets.chomp
+    idiomas.adicionar_palavra(palavra, traducao)
+    puts "Palavra adicionada com sucesso ao vocabulário!"
+  when 2
+    print "Digite a palavra que você gostaria de traduzir: "
+    palavra = gets.chomp
+    traducao = idiomas.traduzir(palavra)
+    if traducao
+      puts "A tradução de '#{palavra}' é '#{traducao}'"
+    else
+      puts "Palavra não encontrada no vocabulário."
+    end
+  when 3
+    puts "Obrigado por usar o programa. Até mais!"
+    break
+  else
+    puts "Opção inválida. Por favor, escolha uma opção válida."
+  end
+
+  puts "\n"  # Adiciona uma linha em branco para melhorar a legibilidade
+end
