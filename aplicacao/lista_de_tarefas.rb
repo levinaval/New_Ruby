194b27ce9f1a9ea34d6a58ed51eb3a22fc53f812
levi <levisantosluna@gmail.com>
2024-04-28 16:09:24 -0300

    Criando Lista de Tarefas.rb

---
+++ aplicacao/lista_de_tarefas.rb
@@ -0,0 +1,28 @@
+class TodoList
+  
+  def initialize
+    @tasks = []
+  end
+
+  def add_task(task)
+    @tasks << task
+  end
+
+  def display_tasks
+    puts "Lista de tarefas:"
+    @tasks.each_with_index do |task, index|
+      puts "#{index + 1}. #{task}"
+    end
+  end
+end
+
+todo_list = TodoList.new
+
+loop do
+  puts "Digite uma tarefa (ou 'sair' para sair): "
+  input = gets.chomp
+  break if input.downcase == "sair"
+
+  todo_list.add_task(input)
+  todo_list.display_tasks
+end
