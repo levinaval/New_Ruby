23ff2eee88e0946493c353d2bca94713603f36fc
levi <levisantosluna@gmail.com>
2024-04-29 04:50:00 -0300

    Concluindo Agenda de Contatos.rb

--- aplicacao/agenda_de_contatos.rb
+++ aplicacao/agenda_de_contatos.rb
@@ -1,5 +1,4 @@
 class Contact
-
   attr_reader :name, :phone_number, :email
 
   def initialize(name, phone_number, email)
@@ -13,6 +12,35 @@ end
 
 class ContactList
   def initialize
-    @Contacts = []
+    @contacts = []
   end
+
+  def add_contact(contact)
+    @contacts << contact
+  end
+
+  def display_contacts
+    puts "Lista de Contatos:"
+    @contacts.each_with_index do |contact, index|
+      puts "#{index + 1}. #{contact}"
+    end
+  end
+end
+
+contact_list = ContactList.new
+
+loop do
+  puts "Digite o nome do contato (ou 'sair' para sair):"
+  name = gets.chomp
+  break if name.downcase == 'sair'
+  
+  puts "Digite o número de telefone:"
+  phone_number = gets.chomp
+  
+  puts "Digite o endereço de e-mail:"
+  email = gets.chomp
+
+  contact = Contact.new(name, phone_number, email)
+  contact_list.add_contact(contact)
+  contact_list.display_contacts
 end
