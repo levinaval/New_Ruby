d7bc5c983d99e18b0107585b6f8b023e2b03c652
levi <levisantosluna@gmail.com>
2024-05-01 13:37:18 -0300

    Gerenciador de Notas

--- aplicacao/gerenciador_de_notas.rb
+++ aplicacao/gerenciador_de_notas.rb
@@ -8,3 +8,39 @@ class Note
     "Categoria: #{@category}/n#{@title}:/n#{@content}"
   end
 end
+
+class NoteManager
+  def initialize
+    @Note = []
+  end
+
+  def add_note(note)
+    @Note << note
+  end
+
+  def display_note
+    puts "Notas:"
+    @notes.each do |note|
+      puts note
+      puts "-" * 20
+    end
+  end
+end
+
+note_manager = NoteManager.new
+
+loop do
+  puts "Digite o título da nota (ou Sair)"
+  title = gets.chomp
+  break if title.downcase == "sair"
+
+  puts "Digite o conteúdo da nota:"
+  category = gets.chomp
+
+  puts "Digite a categoria da nota:"
+  category = gets.chomp
+
+  note = Note.new(title, content, category)
+  note_manager.add_note(note)
+  note_manager.display_note
+end
