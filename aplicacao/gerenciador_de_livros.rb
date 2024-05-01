c55ca67711d2959bcf08520451c3cf43cf03ab02
levi <levisantosluna@gmail.com>
2024-05-01 13:11:08 -0300

    Concluindo Gerenciador de Livros

--- aplicacao/gerenciador_de_livros.rb
+++ aplicacao/gerenciador_de_livros.rb
@@ -9,3 +9,38 @@ attr_reader :title, :author, :genre
     "#{@title} - #{@author} #{@genre}"
   end
 end
+
+class BookManager
+  def initialize
+    @books = []
+  end  
+
+  def add_book(book)
+    @books << book
+  end
+
+  def display_books
+    puts "Lista de livros:"
+    @books.each_with_index do |book, index|
+      puts "#{index + 1}. #{book}"
+    end
+  end
+end
+
+book_manager = BookManager.new
+
+loop do
+  puts "Digite o títolo do livro ( 0 para Sair):"
+    title = gets.chomp.to_s.downcase
+    break if title == 0
+
+    puts "Digite o nome so autor:"
+    author = gets.chomp
+
+    puts "Digite o gênero do livro:"
+    genre = gets.chomp
+
+    book = Book.new(title, author, genre)
+    book_manager.add_book(book)
+    book_manager.display_books
+end
