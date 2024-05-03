578378955d563e8e74fc4eef8a942d39dc477b64
levi <levisantosluna@gmail.com>
2024-05-03 05:16:41 -0300

    Terceira modificação em casatro.rb

--- aplicacao/cadastro.rb
+++ aplicacao/cadastro.rb
@@ -9,24 +9,20 @@ information1 = if(n.length < 4 || n.length > 30) then "Quantidade de caracteres 
 print "Idade: "
 age = gets.chomp.to_i
 
-information2 = if(i <= 4 || i >= 100) then "Idade igual a 0 ou superior a 100 anos!" else "#{i}" end
+information2 = if(age <= 4 || age >= 100) then "Idade igual a 0 ou superior a 100 anos!" else "#{age}" end
 
 print "Endereço: "
 adress = gets.chomp
 
-n3 =
-if(ed.length < 4 || ed.length > 100) then "Quantidade de caracteres abaixo de 4 ou acina de 100" else "#{ed}" end
-
-
+information3 = if(adress.length < 4 || adress.length > 100) then "Quantidade de caracteres abaixo de 4 ou acina de 100" else "#{adress}" end
 
 print "Número: "
-nu = gets.chomp.to_i
+number = gets.chomp.to_i
 
-n4 =
-if(nu <= 4 || nu >= 6) then "Quantidade de caracteres abaixo de 4 ou acina de 6" else "#{nu}" end
+information4 = if(number <= 4 || number >= 6) then "Quantidade de caracteres abaixo de 4 ou acina de 6" else "#{number}" end
 
-puts "-----------------------------------------"
-puts "#{n1}"
-puts "#{n2}"
-puts "#{n3}"
-puts "#{n4}"
+puts " "
+puts "#{information1}"
+puts "#{information2}"
+puts "#{information3}"
+puts "#{information4}"
