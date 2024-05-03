701cb79a5d4772b7dc041ca19ea96a9ab9bd9d1d
levi <levisantosluna@gmail.com>
2024-05-03 16:59:40 -0300

    Nova modificação em casatro.rb

--- aplicacao/cadastro.rb
+++ aplicacao/cadastro.rb
@@ -1,10 +1,10 @@
 #Vamos criar um app simples para efetuar cadastros.
 
-puts "Cadastro de pessoa física"
+puts "Cadastro de pessoa física."
 print "Primeiro nome:"
 name = gets.chomp
 
-information1 = if(name.length < 4 || name.length > 30) then "Quantidade de caracteres abaixo de 4 ou acina de 30!" else "#{name}" end
+information1 = if(name.length < 4 || name.length > 30) then "Quantidade de caracteres abaixo de 4 ou acima de 30." else "#{name}" end
 
 print "Idade: "
 age = gets.chomp.to_i
@@ -19,7 +19,7 @@ information3 = if(adress.length < 4 || adress.length > 100) then "Quantidade de 
 print "Número: "
 number = gets.chomp.to_i
 
-information4 = if(number <= 4 || number >= 6) then "Quantidade de caracteres abaixo de 4 ou acina de 6" else "#{number}" end
+information4 = if(number <= 4 || number >= 6) then "Quantidade de caracteres abaixo de 4 ou acima de 6" else "#{number}" end
 
 puts " "
 puts "#{information1}"
