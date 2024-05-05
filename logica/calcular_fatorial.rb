e5f3ebb895e50c9cf6145b32da33a1c28070eccd
levi <levisantosluna@gmail.com>
2024-05-05 15:59:07 -0300

    Traduzindo para o inglês e adicionando Loop a Calculo Fatorial.rb

--- logica/calcular_fatorial.rb
+++ logica/calcular_fatorial.rb
@@ -1,12 +1,16 @@
-def fatorial(number)
+def factorial(number)
   if number == 0 || number == 1
     return 1
   else
-    return number * fatorial(number - 1)
+    return number * factorial(number - 1)
   end
 end
 
-print "Digite um número para calcular o fatorial: "
-number = gets.chomp.to_i
-resultado = fatorial(number)
-puts "O fatorial de #{number} é #{resultado}"
+loop do
+  print "Digite um número para calcular o fatorial(ou 'Sair'): "
+  number = gets.chomp
+  break if number.downcase == 'sair'
+  number = number.to_i
+  resultado = factorial(number)
+  puts "O fatorial de #{number} é #{resultado}"
+end
