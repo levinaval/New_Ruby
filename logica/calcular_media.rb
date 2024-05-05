b6c63f15b91798c92db651504a142f4576ed0e9d
levi <levisantosluna@gmail.com>
2024-05-05 15:27:45 -0300

    Criando calcular Media.rb

---
+++ logica/calcular_media.rb
@@ -0,0 +1,18 @@
+def calcular_media(n1, n2, n3)
+  media = (n1 + n2 + n3) / 3.0
+  puts "A média dos números é : #{media}"
+end
+
+loop do
+  puts "Digite três números(ou 'Sair'):"
+  print "Primeiro: "
+  n1 = gets.chomp
+  break if n1.downcase == "sair"
+  n1 = n1.to_f
+  print "Segundo: "
+  n2 = gets.chomp.to_f
+  print "terceiro: "
+  n3 = gets.chomp.to_f
+  
+  calcular_media(n1, n2, n3)  
+end
