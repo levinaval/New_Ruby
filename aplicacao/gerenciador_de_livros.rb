d70ddbf68c53ca7a453ec39bcd4d42c102baa8a5
Levi Santos <levisantoslunae@gmail.com>
2024-05-22 22:29:52 -0300

    Comentando Gerenciador de notas.rb

--- aplicacao/gerenciador_de_livros.rb
+++ aplicacao/gerenciador_de_livros.rb
@@ -1,48 +1,59 @@
-#Vamos criar um app simples para gerenciar livros.
+# Vamos criar um app simples para gerenciar livros.
 
+# Definição da classe Book para representar um livro
 class Book
-attr_reader :title, :author, :genre
+  attr_reader :title, :author, :genre  # Cria métodos de leitura para os atributos title, author e genre
 
+  # Método inicializador, chamado quando um novo objeto Book é criado
   def initialize(title, author, genre)
-    @title, @author, @genre = title, author, genre
+    @title, @title.downcase!, @author, @genre = title, author, genre  # Atribui os parâmetros passados aos atributos da instância
   end
 
+  # Método para retornar os detalhes do livro como uma string formatada
   def to_s
-    "#{@title} - #{@author} #{@genre}"
+    "#{@title} - #{@title} - #{@genre}"  # Concatena os detalhes do livro em uma string
   end
 end
 
+# Definição da classe BookManager para gerenciar vários livros
 class BookManager
   def initialize
-    @books = []
+    @books = []  # Inicializa um array vazio para armazenar os livros
   end  
 
+  # Método para adicionar um novo livro à lista de livros
   def add_book(book)
-    @books << book
+    @books << book  # Adiciona o livro ao array de livros
   end
 
+  # Método para exibir todos os livros registrados
   def display_books
-    puts "Lista de livros:"
-    @books.each_with_index do |book, index|
-      puts "#{index + 1}. #{book}"
+    puts "Lista de livros:"  # Imprime o cabeçalho
+    @books.each_with_index do |book, index|  # Itera sobre cada livro no array com índice
+      puts "#{index + 1}. #{book}"  # Imprime o índice e os detalhes do livro
     end
   end
 end
 
+# Cria uma nova instância de BookManager
 book_manager = BookManager.new
 
+# Loop para adicionar livros ao registro
 loop do
-  puts "Digite o títolo do livro ( ou 'Sair'):"
-    title = gets.chomp.to_s.downcase
-    break if title == 'sair'
+  puts "Digite o título do livro (ou 'Sair'):"
+  title = gets.chomp  # Lê o título do livro do usuário
+  break if title.downcase == 'sair'  # Sai do loop se o usuário digitar 'sair'
 
-    puts "Digite o nome do autor:"
-    author = gets.chomp
+  puts "Digite o nome do autor:"
+  author = gets.chomp  # Lê o nome do autor do usuário
 
-    puts "Digite o gênero do livro:"
-    genre = gets.chomp
+  puts "Digite o gênero do livro:"
+  genre = gets.chomp  # Lê o gênero do livro do usuário
 
-    book = Book.new(title, author, genre)
-    book_manager.add_book(book)
-    book_manager.display_books
+  # Cria uma nova instância de Book com os dados fornecidos e adiciona ao gerenciador de livros
+  book = Book.new(title, author, genre)
+  book_manager.add_book(book)
+
+  # Exibe todos os livros registrados após adicionar o novo livro
+  book_manager.display_books
 end
