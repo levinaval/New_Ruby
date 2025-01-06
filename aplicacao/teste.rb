5cf773338e59985c229fb8b6e7ceb50221e7f746
Levi Santos <levisantosluna@gmail.com>
2025-01-06 18:14:42 -0300

    Correção no método editar.

--- aplicacao/teste.rb
+++ aplicacao/teste.rb
@@ -1,32 +1,32 @@
 # PROJETO AGENDA
 # Adicionar, Editar e Remover contato:
-# Contato terão as seguintes informações: nome e telefone
+# Contato terão as seguintes informações: nome e número
 # Podemos ver todos os contatos registrados ou somente um contato;
 
 # Contatos da agenda.
 @agenda =[
-{nome:"Geovane" , telefone:"21971331981"},
-{nome:"Amigo1" , telefone:"1"},
-{nome:"Amigo2" , telefone:"2"},
-{nome:"Amigo3" , telefone:"3"},
-{nome:"Amigo4" , telefone:"4"},
+{nome:"Geovane", numero:"21971331981"},
+{nome:"Amigo1" , numero:"1"},
+{nome:"Amigo2" , numero:"2"},
+{nome:"Amigo3" , numero:"3"},
+{nome:"Amigo4" , numero:"4"},
 ]
 
-# Método para exibir todos os contatos da agenda e imprime o nome e telefone de cada contato.
+# Método para exibir todos os contatos da agenda e imprime o nome e número de cada contato.
 def todos_contatos
     @agenda.each do |contato|
-        putd "#{contato[:nome]} - #{contato[:telefone]}"
+    puts "#{contato[:nome]} - #{contato[:numero]}"
     end
 end
 
 # Método para adicionar um novo contato na agenda.
-# Solicita o nome e telefone do usuário e adiciona ao array @agenda.
+# Solicita o nome e numero do usuário e adiciona ao array @agenda.
 def adicionar_contato
     puts "\nDigite o nome do contato:"
     nome = gets.chomp
     puts "Digite o número do contato:"
     numero = gets.chomp
-    @agenda << { nome: nome, numero: telefone }
+    @agenda << { nome: nome, numero: numero }
     puts "Contato adicionado com sucsso!"
 end
 
@@ -53,11 +53,11 @@ def editar_contato
  
     if contato
         puts "Digite o novo nome (ou precione Enter para manter o atual):"
-        nome = gets.chomp
+        novo_nome = gets.chomp
         contato[:nome] = novo_nome unless novo_nome.empty?
 
         puts "Digite o novo número (ou precione Enter para manter o atual):"
-        nome = gets.chomp
+        novo_numero = gets.chomp
         contato[:numero] = novo_numero unless novo_numero.empty?
 
         puts "Contato atualizado com sucesso!"
@@ -83,7 +83,7 @@ end
 
 loop do 
 
-puts " 1. Contatos\n2. Adicionar Contato\n3. Ver Contrato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contrato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
     codigo= gets.chomp.to_i
     case codigo
     when 0
