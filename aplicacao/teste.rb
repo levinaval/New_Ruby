a295838f32fb02e3b9db7759427c555dc1b42845
Levi Santos <levisantosluna@gmail.com>
2025-01-06 17:49:03 -0300

    desenvolvendo método para editar contato.

--- aplicacao/teste.rb
+++ aplicacao/teste.rb
@@ -5,12 +5,19 @@
 
 # Contatos da agenda.
 @agenda =[
-{nome:"Giovane" , telefone:"21971331981"},
+{nome:"Geovane" , telefone:"21971331981"},
 {nome:"Amigo1" , telefone:"1"},
 {nome:"Amigo2" , telefone:"2"},
 {nome:"Amigo3" , telefone:"3"},
 {nome:"Amigo4" , telefone:"4"},
 ]
+
+# Método para exibir todos os contatos da agenda e imprime o nome e telefone de cada contato.
+def todos_contatos
+    @agenda.each do |contato|
+        putd "#{contato[:nome]} - #{contato[:telefone]}"
+    end
+end
 
 # Método para adicionar um novo contato na agenda.
 # Solicita o nome e telefone do usuário e adiciona ao array @agenda.
@@ -23,12 +30,6 @@ def adicionar_contato
     puts "Contato adicionado com sucsso!"
 end
 
-# Método para exibir todos os contatos da agenda e imprime o nome e telefone de cada contato.
-def todos_contatos
-    @agenda.each do |contato|
-        putd "#{contato[:nome]} - #{contato[:telefone]}"
-    end
-end
 
 # Método para exibir os detalhes de um único contato.
 def ver_contato
@@ -37,13 +38,38 @@ def ver_contato
     contato = @agenda.find { |c| c[:nome].casecmp(nome).zero? }
 
     if contato
+        puts "#{contato[:nome]} - #{contato[:numero]}"
+    else
+        puts "Contato não encontrado."
+    end
+end
+
+
+# Método para editar um contato existente.
+def editar_contato
+    puts "Digite o nome do contato que deseja editar:"
+    nome = gets.chomp
+    contato = @agenda.find { |c| c[:nome].casecmp(nome).zero? }
+ 
+    if contato
+        puts "Digite o novo nome (ou precione Enter para manter o atual):"
+        nome = gets.chomp
+        contato[:nome] = novo_nome unless novo_nome.empty?
+
+        puts "Digite o novo número (ou precione Enter para manter o atual):"
+        nome = gets.chomp
+        contato[:numero] = novo_numero unless novo_numero.empty?
+
+        puts "Contato atualizado com sucesso!"
+
+    else
         puts "Contato não encontrado."
     end
 end
 
 loop do 
 
-puts " 1. Contatos\n2. Adicionar Contato\n3.Ver Contrato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+puts " 1. Contatos\n2. Adicionar Contato\n3. Ver Contrato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
     codigo= gets.chomp.to_i
     case codigo
     when 0
